d080d7fe2b4feadba5a280f26b9e1eb7fc88997e
Din Stolak <dee@Dins-MacBook-Pro.local>
2018-09-26 14:19:38 +0100

    Corrected rspec fasiling test's expectation with help from... updated display of bookmarks to have working links.

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,20 +1,15 @@
-# require 'bookmarks'
-# # describe Bookmarks do
+require 'bookmarks'
+describe Bookmarks do
   
-# #   describe ".all" do
-# #     it 'Should return all bookmarks' do
-# #       expect(Bookmarks.all).to eq([
-# #         "www.reddit.com",
-# #         "www.google.com",
-# #         "www.500px.com"                     
-# #       ])
-# #     end
-# #   end
-# # end
-
-# describe '.all' do
-#   it 'returns a list of bookmarks' do
-#     bookmarks = Bookmarks.all
-
-#   end
-# end
+  describe ".all" do
+    it 'Should return all bookmarks' do
+      bookmarks = Bookmarks.all
+      expect(bookmarks).to eq([
+        "http://www.makersacademy.com",
+        "http://www.google.com",
+        "http://www.destroyallsoftware.com",
+        "duckduckgo.com"
+         ])
+    end
+  end
+end
